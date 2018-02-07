c69b90039fca4600811da389e8ff3e5a3d02a6d3
Clem <zackclemn@gmail.com>
2018-02-07 10:03:53 -0500

    Update GPACalculator.cs

--- GPACalculator.cs
+++ GPACalculator.cs
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 using System;
 using system.ComponentModel;
 using System.Data;
@@ -42,42 +41,6 @@ namespace GPACalculator
           //The overall GPA for that year is shown as decimal GPA, which is equal to the sum / 4 due to their being 4 numbers being added in total.
           GPA = sum / 4;
           //Finally, the average GPA for the year is given out using the GPA variable in place of {0}
-          Write("Your GPA is {0}, thank you for using the GPA Calculator!", GPA);
-          //continue on this line\\
-        }
-    }
-}
->>>>>>> f23c373773ae66930b55ae781223adf94ab15b67
-=======
-using System;
-using system.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
-
-namespace GPACalculator
-{
-  class GPACalculator;
-    {
-        static void main(string[] args)
-        {
-          string name, predictions;
-          int first, second, third, fourth;
-          decimal sum, GPA;
-          Write("Hello and welcome to the weighted GPA Calculator program, please enter your name...");
-          name = ReadLine();
-          Write("Hello {0}! Enter your first quarter GPA:", name);
-          first = ReadLine();
-          Write("Thanks {0}! Now please include your second quarter GPA:",name)
-          second = ReadLine();
-          Write("Thank you {0}! Now please enter your third quarter GPA:", name);
-          third = ReadLine();
-          Write("And please enter your fourth quarter GPA as well:");
-          fourth = ReadLine();
-          sum = first + second + third + fourth;
-          GPA = sum / 4;
           Write("Your GPA is {0}, thank you for using the GPA Calculator!", GPA);
           //continue on this line\\
         }
